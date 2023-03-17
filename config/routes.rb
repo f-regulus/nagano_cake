06f823078868f23d620817c794a165c2194f308d
kenya0927 <123355087+kenya0927@users.noreply.github.com>
2023-03-17 08:22:13 +0000

    [Add]

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   namespace :admin do
+    get 'customers/index'
+    get 'customers/show'
+    get 'customers/edit'
+    get 'customers/update'
+  end
+  namespace :admin do
     get 'homes/top'
   end
   root to: 'homes#top'
@@ -8,20 +14,20 @@ Rails.application.routes.draw do
     sessions: 'public/sessions'
   }
   get 'about' => "homes#about"
-  
+
   scope module: :public do
     resources :deliveries, only: [:index, :edit, :create, :update, :destroy,]
  end
 
-  
+
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
     sessions: "admin/sessions"
   }
-  
+
   namespace :admin do
     root "homes#top"
   end
-  
-  
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
