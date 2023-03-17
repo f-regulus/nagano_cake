fe7a3580e7c44eb7d1aa7876d59e833c9a322e3e
OmoriRiku <oomori3321@gmail.com>
2023-03-17 09:09:20 +0000

    Add商品新規登録、Genreのカラム変更

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,5 +1,16 @@
 class Admin::ItemsController < ApplicationController
+  before_action :authenticate_admin!
   def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to admin_items_path
+    else
+      render :new
+    end
   end
 
   def index
@@ -9,5 +20,15 @@ class Admin::ItemsController < ApplicationController
   end
 
   def edit
+  end
+  
+  private
+  
+  def set_item
+    @item = Item.find(params[:id])
+  end
+  
+  def item_params
+    params.require(:item).permit(:name, :introduction, :price, :image)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_15_081819) do
+ActiveRecord::Schema.define(version: 2023_03_17_090612) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -90,7 +90,7 @@ ActiveRecord::Schema.define(version: 2023_03_15_081819) do
   end
 
   create_table "genres", force: :cascade do |t|
-    t.integer "genre_name", null: false
+    t.string "genre_name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
