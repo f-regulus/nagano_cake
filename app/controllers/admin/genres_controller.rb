6717ecffe0027fad4cce507caafa01b9a4a7545f
OmoriRiku <oomori3321@gmail.com>
2023-03-19 04:43:19 +0000

    Add ジャンル、kaminariの実装

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,6 +1,6 @@
 class Admin::GenresController < ApplicationController
   before_action :authenticate_admin!
-  
+  before_action :edit_genre, only: [:edit, :update]
   
   def index
     @genre = Genre.new
@@ -8,20 +8,33 @@ class Admin::GenresController < ApplicationController
   end
   
   def create
-    @genre = Genre.new(order_params)
-    @genre.save
-    redirect_to admin_genres_path
+    @genre = Genre.new(genre_params)
+    if @genre.save
+      flash[:genre_notice] = 'ジャンルを登録しました'
+      redirect_to admin_genres_path
+    else
+      render :index
+    end
   end
 
   def edit
   end
   
   def update
+    if @genre.update(genre_params)
+      flash[:genre_notice] = '変更を保存しました'
+      redirect_to admin_genres_path
+    else
+      render :edit
+    end
   end
   
   private
+  def edit_genre
+    @genre = Genre.find_by(id: params[:id])
+  end
   
-  def order_params
+  def genre_params
     params.require(:genre).permit(:genre_name)
   end
   
