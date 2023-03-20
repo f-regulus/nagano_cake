a1bd453b54572341227e772fb3e174d0af6a71a9
kenya0927 <123355087+kenya0927@users.noreply.github.com>
2023-03-20 07:27:42 +0000

    [Add]注文履歴一覧

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -17,7 +17,7 @@ class Admin::CustomersController < ApplicationController
   def update
      @customer = Customer.find(params[:id])
      if @customer.update(customer_params)
-     flash[:notice] = 'change completed!!'
+     flash[:notice] = 'You was successfully updated!!'
      redirect_to admin_customer_path
    else
      render "edit"
