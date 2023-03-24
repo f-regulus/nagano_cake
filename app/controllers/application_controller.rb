07c9b5204b11a0107747e635ea62ebc967eebf02
kenya0927 <123355087+kenya0927@users.noreply.github.com>
2023-03-24 09:00:41 +0000

    [Add]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,24 +4,10 @@ class ApplicationController < ActionController::Base
   protected
   def after_sign_in_path_for(resource_or_scope)
     if resource_or_scope.is_a?(Admin)
-        admin_root_path
+        admin_items_path
     else
         customers_mypage_path
     end
-  end
-  
-  def after_sign_out_path_for(resource_or_scope)
-    if resource_or_scope == :user
-        root_path
-    elsif resource_or_scope == :admin
-        new_admin_session_path
-    else
-        root_path
-    end
-  end
-
-  def after_sign_in_path_for(resource)
-    root_path(resource)
   end
 
   def configure_permitted_parameters
