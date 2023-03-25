864844151ce49dbe487904670fa668f8df9cecb1
kaito0112 <123792716+kaito0112@users.noreply.github.com>
2023-03-25 07:40:08 +0000

    商品表示修正

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -2,6 +2,7 @@ class Public::ItemsController < ApplicationController
   def index
     @items = Item.all.page(params[:page]).per(8)
     @genres = Genre.all
+    @items_all = Item.all
   end
 
   def show
